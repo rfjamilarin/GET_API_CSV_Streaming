d47af24f78c1790433116edb7b731c22b935cc63
rfjamilarin <49828717+rfjamilarin@users.noreply.github.com>
2023-11-07 00:22:12 +0800

    Update db.py

--- pythonProject/db.py
+++ pythonProject/db.py
@@ -44,29 +44,3 @@ def generate_schema_by_product(currDatetime,transaction_span_datetime):
 
     curr_transact_csv = result_df
     return curr_transact_csv
-
-def generate_schema_by_city(currDatetime,transaction_span_datetime):
-    global csv_folder
-
-    filtered_dfs = []
-
-    for file in os.listdir(csv_folder):
-        if file.endswith('.csv') and file.startswith('Transaction_'):
-            csv_path = os.path.join(csv_folder, file)
-            df = pd.read_csv(csv_path)
-
-            end_of_day = currDatetime.replace(hour=23, minute=59, second=59)
-
-            df['transactionDatetime'] = pd.to_datetime(df['transactionDatetime'])
-
-            filtered_df = df[
-                (df['transactionDatetime'] <= end_of_day) &
-                (df['transactionDatetime'] >= transaction_span_datetime)
-                ]
-
-            filtered_dfs.append(filtered_df)
-
-    result_df = pd.concat(filtered_dfs, ignore_index=True)
-
-    curr_transact_csv = result_df
-    return curr_transact_csv
